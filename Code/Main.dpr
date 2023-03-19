29da813018c59d208c409697b59c4a64e278fe64
Fedor <Batuto@tut.by>
2023-03-19 22:03:02 +0300

    0.3 Preparation for reverse polish notation

--- Code/Main.dpr
+++ Code/Main.dpr
@@ -7,97 +7,84 @@ program Main;
 uses
   System.SysUtils;
 
-const r=100;
+const
 e = 0.001;
-AvailableButtons:array[1..17] of string = ('sin', 'cos', 'tg', 'ctg', 'ln', 'lg',
-'abs', 'sqr', 'sqrt', 'exp', '+', '-', '*', '/', '=', '(', ')');
-Numbers = ('0123456789.');
+r = 100;
 
 type
-TNumber = Real;
-TProblem = array[1..r] of array [1..2] of TNumber;
+Tmas = array[1..r] of string;
 
-(*
-0 - число
-1 - не число
-*)
-
-procedure Possible;                          //Ничего делать не надо
-var i:integer;
+procedure Enter_The_Problem(var strProblem:string);
 begin
-  writeln('Возможные функции:');
-  for i:=Low(AvailableButtons) to High(AvailableButtons) do
-  begin
-    writeln(i, ': ', AvailableButtons[i]);
-  end;
-end;
-
-procedure Enter_The_Problem(var strProblem:string; var Flag:Boolean);  //В перспективе удалить
-begin
-  Flag:=True;
   writeln('Введите пример:');
   Readln(strProblem);
 end;
 
-procedure Into_Array(strProblem:string; var Problem:TProblem; Flag:Boolean; var Problem_Length:Integer);
+procedure Spaces(var strProblem:string; var Flag:Boolean; var MasProblem:TMas; var LengthMas:Integer);
 var i:Integer;
-TempStr:string;                       //Сделать защиту от двух точек
+NumberOfDots:Byte;
+Temp:string;
 begin
+  LengthMas:=0;
   i:=1;
-  Problem_Length:=0;
-  while i<=length(strProblem) do
+  while (i<=length(strProblem)) and Flag do
   begin
-    TempStr:='';
-    while (i<=Length(strProblem)) and (pos(strProblem[i], Numbers)<>0) do
-    begin
-      TempStr:=TempStr+strProblem[i];
+    Temp:='';
+    NumberOfDots:=0;
+    case strProblem[i] of
+    '0'..'9':
+      begin
+        while (i<=Length(strProblem)) and ((strProblem[i]>='0') and (strProblem[i]<='9') or (strProblem[i] = '.')) do
+        begin
+          Temp:=Temp+strProblem[i];
+          if strProblem[i] = '.' then
+          Inc(NumberOfDots);
+          Inc(i);
+        end;
+        if NumberOfDots>1 then
+        Flag:=False;
+        Inc(LengthMas);
+        MasProblem[LengthMas]:=Temp;
+        Inc(i);
+      end;
+    '+', '-', '*', '/', '^', '(', ')':
+      begin
+        Inc(LengthMas);
+        Temp:=strProblem[i];
+        MasProblem[LengthMas]:=Temp;
+        Inc(i);
+        if i<=Length(strProblem) then
+        Inc(i);
+      end;
+    else
+      Flag:=False;
       Inc(i);
     end;
-    if TempStr<>'' then
-    begin
-      if pos('.', TempStr)<>0 then
-      TempStr[pos('.', TempStr)]:=',';
-      Inc(Problem_Length);
-      Problem[Problem_Length][1]:=strToFloat(TempStr);
-      Problem[Problem_Length][2]:=0;
-    end;
-    Inc(i);
   end;
-end;
-
-procedure Display_Array(Problem:TProblem; Problem_Length:integer);
-var i:integer;
-begin
-  for i:=1 to Problem_Length do
-  begin
-    Writeln(Problem[i][1]:10:3);
-  end;
-end;
-
-procedure Calculating(var Problem:TProblem; Left, Right:Integer; var Flag:boolean);
-var i:integer;
-begin
-
-end;
-
-procedure Find_Brackets(var Problem:TProblem; Problem_Length:Integer;
-var Left, Right:Integer; var Flag:boolean);
- var
- i:Integer;
- f:boolean;
-begin
-
 end;
 
 var
-Problem:Tproblem;
 strProblem:string;
-Problem_Length, Left, Right:integer;
 Flag:boolean;
+MasProblem:TMas;
+LengthMas:Integer;
 begin
-  Possible;
-  Enter_The_Problem(strProblem, Flag);
-  Into_Array(strProblem, Problem, Flag, Problem_Length);
-  Display_Array(Problem, Problem_Length);
+  for var i:=1 to 100 do
+  begin
+    LengthMas:=0;
+    Flag:=True;
+    Enter_The_Problem(strProblem);
+    Spaces(strProblem, Flag, MasProblem, LengthMas);
+    if Flag then
+    begin
+    Writeln('В строку:');
+    Writeln(strProblem);
+    Writeln('В массив:');
+    for var j:=1 to LengthMas do
+      Writeln(MasProblem[j]);
+    end
+    else
+    Writeln('Ошибка');
+  end;
   Readln;
 end.
