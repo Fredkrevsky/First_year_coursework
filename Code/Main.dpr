6581cf1f231360364871910fbe374e428ae9ffc5
Fedor <Batuto@tut.by>
2023-03-21 22:08:55 +0300

    update for Reverse Polish Notation

--- Code/Main.dpr
+++ Code/Main.dpr
@@ -34,6 +34,12 @@ begin
   St:=x;
 end;
 
+procedure Push(var Temp:Pointer; St:Pointer);
+var x:Pointer;
+begin
+
+end;
+
 procedure Print_Stack(var St:Pointer);
 begin
   while St^.Next<>nil do
@@ -43,17 +49,20 @@ begin
   end;
 end;
 
-procedure Spaces(var strProblem:string; var Flag:Boolean; var MasProblem:TMas; var LengthMas:Integer; var St:Pointer);
+procedure Analyze(var strProblem:string; var Flag:Boolean; var MasProblem:TMas; var LengthMas:Integer; var St:Pointer);
 var i:Integer;
 NumberOfDots:Byte;
+LocalFlag:Boolean;
+TempPointer:Pointer;
 Temp:string;
 begin
-  LengthMas:=0;
+  LengthMas:=1;
   i:=1;
   while (i<=length(strProblem)) and Flag do
   begin
     Temp:='';
     NumberOfDots:=0;
+    LocalFlag:=False;
     case strProblem[i] of
     '0'..'9':
       begin
@@ -66,20 +75,78 @@ begin
         end;
         if NumberOfDots>1 then
         Flag:=False;
-        Inc(LengthMas);
         MasProblem[LengthMas]:=Temp;
-        AddToStack(Temp, St);
+        //AddToStack(Temp, St);
       end;
-    '+', '-', '*', '/', '^', '(', ')':
+    '+', '-':
       begin
-        Inc(LengthMas);
-        MasProblem[LengthMas]:=strProblem[i];
         AddToStack(strProblem[i], St);
+        TempPointer:=St;
+        while St^.next <> nil do
+        Push(TempPointer, St);
+      end;
+     '*', '/':
+      begin
+        while (St^.Next<>nil) and (St^data)
+      end;
+     '^':
+      begin
+        AddToStack(strProblem[i], St);
+        TempPointer:=St;
+        while (St^.next <> nil) and (St^.Data <> '^') do
+        Push(TempPointer, St);
+      end;
+    '(':
+      begin
+        AddToStack(strProblem[i], St);
+      end;
+    ')':
+      begin
+        //MasProblem[LengthMas]:=strProblem[i];
+        //AddToStack(strProblem[i], St);
         Inc(i);
+      end;
+    'a'..'z':
+      begin
+        if (i<=length(strProblem)-2) then
+        begin
+        Temp:=strProblem[i]+strProblem[i+1]+strProblem[i+2];
+        if (Temp = 'sin') or (Temp = 'cos') or (Temp = 'exp') or (Temp = 'sqr') or (Temp = 'ctg') then
+          begin
+            //MasProblem[LengthMas]:=Temp;
+            AddToStack(Temp, St);
+            Inc(i, 3);
+            LocalFlag:=True
+          end;
+        end;
+        if (i<=Length(strProblem)-1) and not LocalFlag then
+        begin
+        Temp:=strProblem[i]+strProblem[i+1];
+        if (Temp = 'ln') or (Temp = 'tg') then
+          begin
+            //MasProblem[LengthMas]:=Temp;
+            AddToStack(Temp, St);
+            Inc(i, 2);
+            LocalFlag:=True
+          end;
+        end;
+        if (i<=Length(strProblem)-3) and not LocalFlag then
+        begin
+        Temp:=strProblem[i]+strProblem[i+1]+strProblem[i+2]+strProblem[i+3];
+        if (Temp = 'sqrt') then
+          begin
+            //MasProblem[LengthMas]:=Temp;
+            AddToStack(Temp, St);
+            Inc(i, 4);
+            LocalFlag:=True;
+          end;
+        end;
+        Flag:=LocalFlag;
       end;
     else
       Flag:=False;
     end;
+    Inc(LengthMas);
   end;
 end;
 
@@ -97,7 +164,7 @@ begin
     Enter_The_Problem(strProblem);
     New(St);
     St^.Next:=nil;
-    Spaces(strProblem, Flag, MasProblem, LengthMas, St);
+    Analyze(strProblem, Flag, MasProblem, LengthMas, St);
     if Flag then
     begin
     Writeln('В строку:');
